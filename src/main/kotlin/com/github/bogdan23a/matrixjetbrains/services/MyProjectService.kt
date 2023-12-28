05f266a98474b3a9113363f44071c4d9362999f9
GitHub Action <action@github.com>
2023-12-28 17:57:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/bogdan23a/matrixjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bogdan23a/matrixjetbrains/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bogdan23a.matrixjetbrains.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bogdan23a.matrixjetbrains.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/bogdan23a/matrixjetbrains/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bogdan23a/matrixjetbrains/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bogdan23a.matrixjetbrains.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bogdan23a.matrixjetbrains.MyBundle
+import com.github.bogdan23a.matrixjetbrains.services.MyProjectService
 import javax.swing.JButton
 
 
